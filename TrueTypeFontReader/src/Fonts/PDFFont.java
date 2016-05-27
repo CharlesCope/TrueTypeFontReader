941d4ffd8672c695525d8a515f61cbc56b0e8864
Charles Cope <charlescope@msn.com>
2016-05-27 12:38:34 -0400

    Working on the Font Bounding Box.

--- TrueTypeFontReader/src/Fonts/PDFFont.java
+++ TrueTypeFontReader/src/Fonts/PDFFont.java
@@ -1,8 +1,13 @@
 package Fonts;
-
+/** Notes on Java Data Types.
+ * byte: Byte data type is an 8-bit signed,Minimum value is -128, Maximum value is 127 (inclusive) 
+ * short:Short data type is a 16-bit signed,Minimum value is -32,768, Maximum value is 32,767 (inclusive)
+ * int:Int data type is a 32-bit signed,Minimum value is - 2,147,483,648, Maximum value is 2,147,483,647(inclusive) 
+ * */
 public class PDFFont {
 	
 	private String strBaseFontName = "";
+	private String strFontBBox = "";
 	private boolean blnFixedPitchFlag = false;
 	private boolean blnSerifFlag = false;
 	private boolean blnSymbolicFlag = false;
@@ -12,18 +17,34 @@ public class PDFFont {
 	private boolean blnAllCapFlag = false;
 	private boolean blnSmallCapFlag = false;
 	private boolean blnForceBoldFlag = false;
+	private int intUnitsPerEm = 0;
+	private int BBoxLowerLeftx = 0;
+	private int BBoxLowerLefty = 0;
+	private int BBoxUpperRightx = 0;
+	private int BBoxUpperRighty = 0;
+	
 	
 	/**The Constructor*/
 	public PDFFont(){}
-	
+	/** PDF Notes On Subject. 
+	 * The PostScript name for the value of BaseFont is determined in one of two ways:
+	 *  Use the PostScript name that is an optional entry in the “name” table of the TrueType font. 
+	 *  
+	 *  In the absence of such an entry in the “name” table, derive a PostScript name
+	 *  from the name by which the font is known in the host operating system. On a
+	 *  Windows system, the name is based on the lfFaceName field in a LOGFONT
+	 *  structure; in the Mac OS, it is based on the name of the FOND resource. If the
+	 *  name contains any spaces, the spaces are removed.        
+	 */
 	public String getFontBaseName(){return strBaseFontName;}
+	/** The value of the Flags entry in a font descriptor is an unsigned 32-bit(big-endian integer containing
+	 * flags specifying various characteristics of the font.
+	 * Example "0000 0000 0000 0100 0000 0000 0010 0010"
+	 * Decimal Value = 262178
+	 * Flags Set 2,6,19
+	*/ 
 	public String getFontDescriptorFlags(){
-		/** The value of the Flags entry in a font descriptor is an unsigned 32-bit(big-endian integer containing
-		 * flags specifying various characteristics of the font.
-		 * Example "0000 0000 0000 0100 0000 0000 0010 0010"
-		 * Decimal Value = 262178
-		 * Flags Set 2,6,19
-		*/ 
+		
 		// big-endian string
 		StringBuilder str32Flag = new StringBuilder("00000000000000000000000000000000");
 		final int RADIX = 10;
@@ -45,7 +66,40 @@ public class PDFFont {
 		return "/Flags " + intFlagsVaule;
 		
 	}
-	
+	/** Sources for the code I used...
+	 *https://github.com/zendframework/zf1/blob/master/library/Zend/Pdf/Resource/Font/FontDescriptor.php Line:116
+	 * http://www.websupergoo.com/helppdfnet/default.htm?page=source%2Fdefault.htm
+	 * This XRect corresponds to the xMin, xMax, yMin and yMax entries in the 'head' TrueType table.
+	 * Distances are measured up and right in points. Points are a traditional measure for print work and there are 72 points in an inch 
+	 * Notes on Font Bounding Box
+	 * PDF Rectangles:
+	 * rectangle is written as an array of four numbers giving the coordinates of a pair of 
+	 * diagonally opposite corners. Typically, the array takes the form [ lowerLeftx lowerLefty upperRightx upperRighty ]
+	 * */
+	public String getFontBBox(){
+		int lowerLeftx = 0;
+		int lowerLefty = 0;
+		int upperRightx = 0;
+		int upperRighty =0;
+		
+		lowerLeftx = (int) toEmSpace(BBoxLowerLeftx);
+		lowerLefty = (int) toEmSpace(BBoxLowerLefty);
+		upperRightx = (int) toEmSpace(BBoxUpperRightx);
+		upperRighty= (int) toEmSpace(BBoxUpperRighty);
+		
+		strFontBBox = "/FontBBox [";
+		strFontBBox += String.valueOf(lowerLeftx) + " ";
+		strFontBBox += String.valueOf(lowerLefty) + " ";
+		strFontBBox += String.valueOf(upperRightx) + " ";
+		strFontBBox += String.valueOf(upperRighty) + "] ";
+		
+		return strFontBBox;
+		
+	}
+	public int[] getGlyphWidthsToPDFWidths(){
+		return null;
+		
+	}
 	public void setFontBaseName(String strName){strBaseFontName = strName;}
 	
 	/** Our Flags for the font Descriptor */
@@ -67,7 +121,28 @@ public class PDFFont {
 	
 	public void setForceBoldFlag(boolean setFlag){blnForceBoldFlag = setFlag;}
 	
-	// Need a toString Method for debugging and development
+	public int getUnitsPerEm() {return intUnitsPerEm;}
+	
+	public void setUnitsPerEm(int UnitsPerEm) {intUnitsPerEm = UnitsPerEm;}
+	
+	public int getBoundingBoxLowerLeftx() {return BBoxLowerLeftx;}
+	public int getBoundingBoxLowerLefty() {return BBoxLowerLefty;}
+	public int getBoundingBoxUpperRightx() {return BBoxUpperRightx;}
+	public int getBoundingBoxUpperRighty() {return BBoxUpperRighty;}
+	
+	public void setBoundingBoxLowerLeftx(int bBoxLowerLeftx) {BBoxLowerLeftx = bBoxLowerLeftx;}
+	public void setBoundingBoxLowerLefty(int bBoxLowerLefty) {BBoxLowerLefty = bBoxLowerLefty;}
+	public void setBoundingBoxUpperRightx(int bBoxUpperRightx) {BBoxUpperRightx = bBoxUpperRightx;}
+	public void setBoundingBoxUpperRighty(int bBoxUpperRighty) {BBoxUpperRighty = bBoxUpperRighty;}
+	
+	/** https://github.com/zendframework/zf1/blob/master/library/Zend/Pdf/Resource/Font.php Line:522
+	 *  If the font's glyph space is not 1000 units per em, converts the value. */
+    public double toEmSpace(double dblValue){
+	        if (intUnitsPerEm == 1000) {return dblValue;}
+	        return Math.ceil((dblValue / intUnitsPerEm) * 1000);    // always round up
+	    }
+	
+	/** Need a toString Method for debugging and development */
 	
 	public String toString(){
 		String JavaNewLine = System.getProperty("line.separator");
@@ -75,7 +150,7 @@ public class PDFFont {
 		
 		strToString = "BaseFont Name >> " + strBaseFontName + JavaNewLine;
 		strToString += "Flags Values >> " + getFontDescriptorFlags() + JavaNewLine;
-		
+		strToString += "Font Bounding Box >> " + getFontBBox() + JavaNewLine;
 		return strToString;
 		
 	}
--- TrueTypeFontReader/src/Fonts/fontToPDFfont.java
+++ TrueTypeFontReader/src/Fonts/fontToPDFfont.java
@@ -2,6 +2,7 @@ package Fonts;
 
 import Fonts.table.NameTable;
 
+
 /** Wanted a place to run code not tied to the GUI  */
 public class fontToPDFfont {
 	private static String OS = null;
@@ -12,22 +13,17 @@ public class fontToPDFfont {
 		ChcFont myChcFont = new ChcFont().create(strFile);
 		// Then create the PDFFont Object to get data from True Type Font Object
 		PDFFont myPDFFont = new PDFFont();
-		
-		/** The PostScript name for the value of BaseFont is determined in one of two ways:
-		 *  Use the PostScript name that is an optional entry in the “name” table of the TrueType font. 
-		 *  
-		 *  In the absence of such an entry in the “name” table, derive a PostScript name
-		 *  from the name by which the font is known in the host operating system. On a
-		 *  Windows system, the name is based on the lfFaceName field in a LOGFONT
-		 *  structure; in the Mac OS, it is based on the name of the FOND resource. If the
-		 *  name contains any spaces, the spaces are removed.        
-		 */
-		
 	
+		myPDFFont.setUnitsPerEm(myChcFont.getHeadTable().getUnitsPerEm());
+		myPDFFont.setBoundingBoxLowerLeftx(myChcFont.getHeadTable().getXMin());
+		myPDFFont.setBoundingBoxLowerLefty(myChcFont.getHeadTable().getYMin());
+		myPDFFont.setBoundingBoxUpperRightx(myChcFont.getHeadTable().getXMax());
+		myPDFFont.setBoundingBoxUpperRighty(myChcFont.getHeadTable().getYMax());
+		
 		String strBaseFontName = myChcFont.getNameTable().getRecord(NameTable.namePostscriptName);
 		if(strBaseFontName.isEmpty() == false){myPDFFont.setFontBaseName(strBaseFontName);}
 		else{
-			// Do this incase the Postscript value is empty.
+			// Do this in case the Postscript value is empty.
 			if(isWindows() == true){}
 			if(isMac() == true){}
 		}
@@ -49,11 +45,14 @@ public class fontToPDFfont {
 		return myPDFFont;
 		
 	}
+	
 	public static String getOsName(){
 		// The operating system of the host that my Java program is running 
 		if(OS == null) { OS = System.getProperty("os.name"); }
 	      return OS;
 	}
+	
 	public static boolean isWindows(){return getOsName().startsWith("Windows");}
+	
 	public static boolean isMac(){return getOsName().startsWith("Mac");}
 }
