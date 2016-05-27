86db66a3f955b201bda374fe51ce342b16ba97bb
Charles Cope <charlescope@msn.com>
2016-05-27 12:59:22 -0400

    Testing Commit

--- TrueTypeFontReader/src/Fonts/PDFFont.java
+++ TrueTypeFontReader/src/Fonts/PDFFont.java
@@ -77,7 +77,7 @@ public class PDFFont {
 	 * diagonally opposite corners. Typically, the array takes the form [ lowerLeftx lowerLefty upperRightx upperRighty ]
 	 * */
 	public String getFontBBox(){
-		// Results not matching data in file.
+		// Results not matching data in file.test
 		int lowerLeftx = 0;
 		int lowerLefty = 0;
 		int upperRightx = 0;
